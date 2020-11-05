d0aa32cf698985c5e7e36eca5ee337118780db1c
Daniil Borisov <73256433+Taprich@users.noreply.github.com>
2020-11-05 21:21:32 +0300

    Add files via upload

---
+++ ControlWork/ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <Windows.h>
+using std::cout; 
+using std::cin;
+
+int main(void)
+{
+	char ch;
+	FILE* File;
+	char filename[40];
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << " Введите имя файла: ";
+	cin.getline(filename, 39);//считываем имя файла
+	File = fopen(filename, "r");//открываем файл для записи
+	cout << " Данные в файле: ";
+	while ((ch = fgetwc(File)) != EOF) //посимвольное чтение
+	{
+		SetConsoleCP(CP_UTF8);//кодировка 
+		SetConsoleOutputCP(CP_UTF8);//в UTF8
+		printf("%c ", ch);
+	}
+	cout << "\n";
+	fclose(File);//закрываем файл
+	system("pause");
+}
